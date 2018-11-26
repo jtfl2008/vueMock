adab5ac000a107b22543f9b7bd3f97ef69879df6
jtfl2008 <jtfl2008@gmail.com>
2018-11-26 23:34:25 +0800

    文章详情页面路由添加

--- src/router/router.js
+++ src/router/router.js
@@ -1,5 +1,6 @@
 const Home = () => import('@/views/Home.vue')
 const About = () => import('@/views/About.vue')
+const Article = () => import('@/views/Article.vue')
 const routes = [
   {
     path: '/',
@@ -15,6 +16,14 @@ const routes = [
     component: About,
     meta: {
       title: '关于我们'
+    }
+  },
+  {
+    path: '/article/:id',
+    name: 'article',
+    component: Article,
+    meta: {
+      title: '文章详情'
     }
   }
 ]
