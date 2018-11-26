f889ea17c5722d35d21a114d1c5fee58e84547a4
jtfl2008 <jtfl2008@gmail.com>
2018-11-26 23:35:09 +0800

    数据调试

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,25 +1,50 @@
 <template>
   <div class="home">
-    <button @click="getList">请求数据</button>
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <button @click="getArticles">请求全部数据</button> -->
+    <!-- <button @click="getArticle">请求单条数据</button> -->
+    <button @click="createArticle">创建单条数据</button>
+    <ul>
+      <router-link tag="li"
+                   v-for="item in articles"
+                   :key="item._id"
+                   :to="{name: 'article', params:{id: item._id}}">{{item.title}} </router-link>
+    </ul>
+    <img alt="Vue logo"
+         src="../assets/logo.png">
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    // HelloWorld
   },
-  methods: {
-    async getList() {
-      let data = await this.$api.user.getList()
-      console.log(data)
+  data () {
+    return {
+      articles: []
     }
   },
+  mounted () {
+    this.getArticles()
+  },
+  methods: {
+    async getArticles () {
+      let data = await this.$api.user.getArticles()
+      this.articles = data
+    },
+    async createArticle () {
+      let params = {
+        name: 'test',
+        author: 'liuiui'
+      }
+      let data = await this.$api.user.createArticle(params)
+      console.log(data)
+    }
+  }
 }
 </script>
