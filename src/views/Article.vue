6386d90b983b67be9de8c791aafa1c68644b6215
jtfl2008 <jtfl2008@gmail.com>
2018-11-26 23:34:47 +0800

    详情页面添加

---
+++ src/views/Article.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    {{article.title}}
+    {{article.author}}
+    <div v-html="article.content"></div>
+
+  </div>
+</template>
+
+<script>
+import { setTitle } from '../utils'
+export default {
+  data () {
+    return {
+      article: {}
+    }
+  },
+  mounted () {
+    this.getArticle()
+  },
+  methods: {
+    async getArticle () {
+      let params = {
+        id: this.$route.params.id
+      }
+      let data = await this.$api.user.getArticle(params)
+      this.article = data
+      setTitle(`${this.$route.meta.title} -- ${this.article.title}`)
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
